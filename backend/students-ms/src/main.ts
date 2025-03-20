10feaf1aeaea9189000b298291a3e87db372c484
Juan <josemargri3@gmail.com>
2025-03-20 13:55:30 -0500

    feature: Create a Resource for students with DTO Creation and Pipes

--- backend/students-ms/src/main.ts
+++ backend/students-ms/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Listening on port:', process.env.PORT ?? 'no hay');
+
+  // Configurar globalmente los pipes de validación
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no definidas en los DTOs
+      forbidNonWhitelisted: true, // Lanza error si se envían propiedades desconocidas
+      transform: true, // Convierte los tipos automáticamente (por ejemplo, string a Date)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
