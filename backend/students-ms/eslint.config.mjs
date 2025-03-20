241370fce89235ad0713ebc83f324e644165faa1
Juan <josemargri3@gmail.com>
2025-03-20 01:08:31 -0500

    prettier: Modify prettier config

--- backend/students-ms/eslint.config.mjs
+++ backend/students-ms/eslint.config.mjs
@@ -29,7 +29,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      // Agregamos la configuración para evitar el error de ␍
+      'prettier/prettier': ['error', { endOfLine: 'lf' }],
     },
   },
 );
