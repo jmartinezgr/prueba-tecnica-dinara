3aeefafaf3d8ada27c9c48e73fc450497ea191ff
Juan <josemargri3@gmail.com>
2025-03-22 18:10:56 -0500

    chore: Struct the courses resource

--- backend/courses-ms/src/main.ts
+++ backend/courses-ms/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
+  console.log(
+    'Students Microservice is running on port:',
+    process.env.PORT ?? 3002,
+  );
 }
 bootstrap();
