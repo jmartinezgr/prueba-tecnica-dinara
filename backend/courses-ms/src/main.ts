c69c241942ea35758e114b40b6960b81d3a5512d
Juan <josemargri3@gmail.com>
2025-03-22 18:34:09 -0500

    feature: Initilizate config module for the course microservice

--- backend/courses-ms/src/main.ts
+++ backend/courses-ms/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
-  console.log(
-    'Students Microservice is running on port:',
-    process.env.PORT ?? 3002,
-  );
+  await app.listen(envs.port);
+  console.log('Students Microservice is running on port:', envs.port);
 }
 bootstrap();
