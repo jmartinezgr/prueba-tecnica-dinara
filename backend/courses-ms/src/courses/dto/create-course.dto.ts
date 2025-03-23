22eca045f2decd12ca44dda68a25b608636aca3f
Juan <josemargri3@gmail.com>
2025-03-22 22:04:17 -0500

    feature: Courses DTOs created

--- backend/courses-ms/src/courses/dto/create-course.dto.ts
+++ backend/courses-ms/src/courses/dto/create-course.dto.ts
@@ -1,1 +1,23 @@
-export class CreateCourseDto {}
+import { IsString, IsNotEmpty, IsInt, Min } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  professor: string;
+
+  @IsInt()
+  @Min(1)
+  maxSlots: number;
+
+  @IsInt()
+  @Min(0)
+  enrolledStudents: number = 0;
+}
