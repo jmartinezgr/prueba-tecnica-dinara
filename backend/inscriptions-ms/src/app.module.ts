4ae860022fd0546d2b31e3e7920ac4ad04e14902
Juan <josemargri3@gmail.com>
2025-03-23 01:30:17 -0500

    feature: Struct the inscrptions microservices to manage the request

--- backend/inscriptions-ms/src/app.module.ts
+++ backend/inscriptions-ms/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InscriptionsModule } from './inscriptions/inscriptions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InscriptionsModule],
 })
 export class AppModule {}
